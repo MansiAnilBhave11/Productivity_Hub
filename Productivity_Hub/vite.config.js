0c7ed81fe11d21e475af611928f9aaca615f5553
MansiAnilBhave11 <mansianilbhave@gmail.com>
2025-07-06 18:55:53 +0530

    vercel file edit

--- Productivity_Hub/vite.config.js
+++ Productivity_Hub/vite.config.js
@@ -11,4 +11,13 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
